ac2f8fad76c375c0773d87afaf58236c0e941698
Eamo5 <eamonpowell@hotmail.com>
2019-01-29 11:39:29 +1100

    wake_gestures: fix merge error

--- drivers/input/touchscreen/sec_ts/y771/sec_ts.c
+++ drivers/input/touchscreen/sec_ts/y771/sec_ts.c
@@ -3232,14 +3232,14 @@ static void sec_ts_input_close(struct input_dev *dev)
 
 	ts->pressure_setting_mode = 0;
 
-	if (ts->prox_power_off) {
-		sec_ts_stop_device(ts);
-	} else {
 #ifdef CONFIG_WAKE_GESTURES
 		if (s2w_switch || dt2w_switch)
 			enable_irq_wake(ts->client->irq);
 		else
 #endif
+	if (ts->prox_power_off) {
+		sec_ts_stop_device(ts);
+	} else {
 		if (ts->lowpower_mode) {
 
 			if (ts->use_sponge) {
